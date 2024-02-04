a936dd027e2b413ad2b412ffd98c99a2ec1aac10
sadishaaaa <sthasadisha2019@gmail.com>
2024-02-04 20:50:22 +0545

    query inside model added for user details

--- server/src/model/users.js
+++ server/src/model/users.js
@@ -7,10 +7,35 @@ export const registerUser = async (data) => {
       INSERT INTO users (
         username,
         email,
-        "password"
-      ) VALUES ($1, $2, $3)
+        "password",
+        ciy,
+        contact_no
+      ) VALUES ($1, $2, $3,$4,$5)
       RETURNING *`,
-      [data.username, data.email, data.password]
+      [data.username, data.email, data.password, data.city, data.contact_no]
+    );
+
+    return result;
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const userDetails = async (data) => {
+  try {
+    const result = await connectdb.query(
+      `
+      INSERT INTO users (
+        cover_photo,
+        profile_photo,
+        bio,
+        social_links
+      ) VALUES ($1, $2, $3, $4)
+      RETURNING *`,
+      [data.cover_photo, data.profile_photo, data.bio, data.social_links]
     );
 
     return result;
