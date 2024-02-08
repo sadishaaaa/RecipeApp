9056b9cb34681fee69061b7e64b8bdb6d126e8cf
sadishaaaa <sthasadisha2019@gmail.com>
2024-02-08 13:17:10 +0545

    step-1 designed

--- client/src/Component/Register/Stepper.jsx
+++ client/src/Component/Register/Stepper.jsx
@@ -61,6 +61,7 @@ const Stepper = ({ steps, currentSteps }) => {
             : "flex items-center"
         }
       >
+        {console.log(step)}
         <div className="relative flex flex-col items-center text-teal-600">
           <div
             className={`rounded-full transition duration-500 ease-in-out border-2 border-grey-300 h-12 w-12 flex items-center justify-center py-3 ${
--- client/src/Component/Register/Steps/Step1.jsx
+++ client/src/Component/Register/Steps/Step1.jsx
@@ -1,5 +1,108 @@
+import { useState } from "react";
+
 const Step1 = () => {
-  return <div className="flex flex-col"></div>;
+  let [username, setUsername] = useState("");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [contactNo, setContactNo] = useState(0);
+  let [city, setCity] = useState("");
+  return (
+    <div className="flex flex-col">
+      <form className=" border-gray-200 border-2  rounded p-10">
+        <div className="mb-4">
+          <label
+            htmlFor="username"
+            className="block text-md font-medium pb-3 text-gray-600"
+          >
+            UserName:
+          </label>
+          <input
+            id="name"
+            type="text"
+            placeholder="UserName"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-md font-medium pb-3 text-gray-600"
+          >
+            Email:
+          </label>
+          <input
+            id="name"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-md font-medium pb-3 text-gray-600"
+          >
+            Password
+          </label>
+          <input
+            id="name"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="city"
+            className="block text-md font-medium pb-3 text-gray-600"
+          >
+            City
+          </label>
+          <input
+            id="name"
+            type="text"
+            placeholder="City"
+            value={city}
+            onChange={(e) => {
+              setCity(e.target.value);
+            }}
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="contactNo"
+            className="block text-md font-medium pb-3 text-gray-600"
+          >
+            Contact Number:
+          </label>
+          <input
+            id="name"
+            type="number"
+            placeholder="Contact Number"
+            value={contactNo}
+            onChange={(e) => {
+              setContactNo(e.target.value);
+            }}
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Step1;
